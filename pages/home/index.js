c3f14198c7667e94b4d9a76e426280e3df10483b
Lucas Araujo <lucas984618740@gmail.com>
2022-10-14 14:23:02 -0300

    entrega1

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,1 +1,146 @@
 /* Desenvolva sua lógica aqui...*/
+
+let objSearch
+
+let arrTree =[]
+
+
+
+if(localStorage.getItem("arrTree") ){
+
+    arrTree= JSON.parse(localStorage.getItem("arrTree"))
+    
+}
+
+
+
+async function getUser(user){
+
+try{
+    let data = await fetch(`https://api.github.com/users/${user}`)
+    let datajason = await data.json()
+    let button1 = document.querySelector("#button1")
+    let NotFundText =document.querySelector(".userNotFound")
+
+
+
+    if(data.status === 200){
+        button1.classList ="buttonShearchUser"
+        NotFundText.innerText =""
+        button1.innerText="Ver perfil do github"
+        button1.disabled = false
+        objSearch = datajason
+
+        
+    } else if(data.status === 404){
+        button1.classList = "buttonShearchUserDefault"
+        NotFundText.innerText = "usuario nao encontrado"
+        button1.innerText="Ver perfil do github"
+        button1.disabled = true
+    } 
+    
+
+}catch{
+
+    console.log(" error")
+    
+}
+
+}
+
+
+function submitUser(){
+    let inputHome = document.querySelector(".inputHome")
+    let button1 = document.querySelector(".buttonShearchUserDefault")
+    
+
+    inputHome.addEventListener("keyup",()=>{
+
+        button1.innerText=""
+
+        button1.classList ="buttonShearchUserloading"
+        getUser(inputHome.value)
+
+
+    } )
+
+}
+
+submitUser()
+
+
+function buttonClick(){
+
+    let button1 = document.querySelector("#button1")
+
+    button1.addEventListener("click",()=>{
+        
+        
+        arrTree.unshift(objSearch)
+        if(arrTree.length > 3 ){
+            arrTree.splice(3,1)
+        }
+
+        let arrjson =JSON.stringify(arrTree)
+        localStorage.setItem("arrTree",arrjson)
+        
+        
+        let objJson = JSON.stringify(objSearch)
+        localStorage.setItem("objSearched",objJson)
+
+
+        
+
+        window.location.href ="../profile/index.html"
+
+        
+
+        console.log(arrTree)
+    })
+
+    
+
+}
+buttonClick()
+
+function recently(){
+    arrTree.forEach(element =>{
+
+        console.log(element)
+        let ul = document.querySelector(".listResentUser")
+        let li = document.createElement("li")
+        let a = document.createElement("a")
+        let img = document.createElement("img")
+        let span = document.createElement("span")
+
+
+        li.classList.add("resentUser")
+        a.classList.add("linkUser")
+        img.classList.add("imgUser")
+        span.classList.add("spanAccess")
+
+
+        a.addEventListener("click",()=>{
+            let objJson = JSON.stringify(element)
+            localStorage.setItem("objSearched",objJson)
+        })
+        a.href ="../profile/index.html"
+        
+        img.src = element.avatar_url
+        span.innerText ="Acessar este perfil"
+
+        ul.appendChild(li)
+        li.appendChild(a)
+        a.append(img, span)
+
+
+       
+        
+    })
+}
+
+recently()
+
+
+
+
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,1 +1,111 @@
 /* Desenvolva sua lógica aqui...*/
+
+let objCurrent = JSON.parse( localStorage.getItem("objSearched"))
+
+
+
+
+function headerPageGenerator(){
+    let imgPerfil =document.querySelector(".imgPerfil")
+    let userName =document.querySelector(".userName")
+    let userDesc = document.querySelector(".userDesc")
+    let buttonEmail = document.querySelector(".buttonEmail")
+    let buttonInnerUser = document.querySelector(".buttonInnerUser")
+
+    imgPerfil.src = objCurrent.avatar_url
+    userName.innerText = objCurrent.name
+    userDesc.innerText = objCurrent.bio
+
+
+    buttonEmail.addEventListener("click", ()=>{
+
+        if(objCurrent.email === null){
+
+            window.alert("Esta conta nao tem Email cadastrado")
+            
+        }else{
+
+            window.location.href =`mailto:${objCurrent.email}`
+        }
+        
+    })
+
+    buttonInnerUser.addEventListener("click",()=>{
+        window.location.href ="../home/index.html"
+    })
+
+}
+
+headerPageGenerator()
+
+async function CardsPageGenerator(){
+
+  
+let cards = document.querySelector(".cards")
+
+ let res1 = await fetch(objCurrent.repos_url)
+ let repos = await res1.json()
+
+ repos.forEach( element =>{
+
+  
+
+  let card = document.createElement("div")
+  let h2Card = document.createElement("h2")
+  let textCard = document.createElement("p")
+  let buttonRep = document.createElement("button")
+  let buttonDemo = document.createElement("button")
+  let divButtonsCard = document.createElement("div")
+
+  card.classList.add("card")
+  h2Card.classList.add("h2Card")
+  textCard.classList.add("textCard")
+  divButtonsCard.classList.add("divButtonsCard")
+  buttonRep.classList = "buttonRep buttonDefult"
+  buttonDemo.classList= "buttonDemo buttonDefult"
+
+  h2Card.innerText=element.name
+  textCard.innerText=element.description
+  buttonRep.innerText="Repositório"
+  buttonDemo.innerText="Demo"
+
+
+  buttonRep.addEventListener("click",()=>{
+
+   window.location.href = element.html_url
+
+    
+  })
+
+
+  cards.appendChild(card)
+  card.append(h2Card, textCard, divButtonsCard)
+  divButtonsCard.append(buttonRep,buttonDemo)
+
+
+  
+
+ })
+
+}
+
+CardsPageGenerator()
+
+/* <main class="">
+
+<section class="cards">
+  <div class="card">
+    <h2 class="h2Card">
+      Project Module 2 - Kenzie
+    </h2>
+    <p class="textCard">
+      Various versions have evolved over the years, sometimes by accident, sometimes on purpose injected humour and the like
+    </p>
+    
+    <button class="buttonRep buttonDefult">Repositório</button>
+    <button class="buttonDemo buttonDefult">Demo</button>
+
+  </div>
+</section>
+
+</main> */
